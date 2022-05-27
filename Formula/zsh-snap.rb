94762caf24727c22c4de3240b6c8f64a27378d59
Colby Swandale <me@colby.fyi>
2022-05-27 15:16:05 +1000

    Create zsh-snap.rb

---
+++ Formula/zsh-snap.rb
@@ -0,0 +1,6 @@
+class ZshSnap < Formula
+  desc "Znap is the light-weight Git repo manager for Zsh that's easy to grok. CLI"
+  homepage "https://github.com/marlonrichert/zsh-snap"
+
+  head "https://github.com/marlonrichert/zsh-snap"
+end
